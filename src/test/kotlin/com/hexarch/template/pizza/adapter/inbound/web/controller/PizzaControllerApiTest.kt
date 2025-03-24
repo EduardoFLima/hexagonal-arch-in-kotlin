69dc5280f3a4fec34953f567356c4506257402e5
Edu Lima <eduefi@gmail.com>
2025-03-24 08:43:35 +0100

    add test for when parameters are missing when creating a pizza

--- src/test/kotlin/com/hexarch/template/pizza/adapter/inbound/web/controller/PizzaControllerApiTest.kt
+++ src/test/kotlin/com/hexarch/template/pizza/adapter/inbound/web/controller/PizzaControllerApiTest.kt
@@ -5,6 +5,7 @@ import com.hexarch.template.pizza.domain.model.entity.Pizza
 import com.hexarch.template.pizza.domain.model.value.PizzaType.NEAPOLITAN
 import com.hexarch.template.pizza.port.outbound.PizzaPersistencePort
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.mockito.Mockito.`when`
 import org.mockito.kotlin.eq
 import org.springframework.beans.factory.annotation.Autowired
@@ -33,42 +34,63 @@ class PizzaControllerApiTest(
     @MockitoBean
     private lateinit var pizzaPersistencePort: PizzaPersistencePort
 
-    @Test
-    fun `SHOULD create a pizza WHEN parameters are valid`() {
-        val pizzaName = "Mama mia"
-        val pizzaType = NEAPOLITAN
+    @Nested
+    inner class CreatePizzaApi {
 
-        // TODO remove it once database is set
-        val newPizzaId = UUID.randomUUID()
-        `when`(pizzaPersistencePort.persistPizza(eq(Pizza(name = pizzaName, type = NEAPOLITAN)))).thenReturn(
-            Pizza(newPizzaId, pizzaName, NEAPOLITAN)
-        )
+        @Test
+        fun `SHOULD create a pizza WHEN parameters are valid`() {
+            val pizzaName = "Mama mia"
+            val pizzaType = NEAPOLITAN
 
-        val mockMvcResult = mockMvc.perform(
-            post("/v1/pizzas")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(
-                    """
+            // TODO remove it once database is set
+            val newPizzaId = UUID.randomUUID()
+            `when`(pizzaPersistencePort.persistPizza(eq(Pizza(name = pizzaName, type = NEAPOLITAN)))).thenReturn(
+                Pizza(newPizzaId, pizzaName, NEAPOLITAN)
+            )
+
+            val mockMvcResult = mockMvc.perform(
+                post("/v1/pizzas")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(
+                        """
                     {
                         "name": "$pizzaName",
                         "type": "${pizzaType.name}"
                     }
                 """.trimIndent()
-                )
-        )
-            .andExpect(status().isCreated)
-            .andReturn()
-
-        val contentAsString = mockMvcResult.response.contentAsString
-        assertThat(contentAsString).isNotEmpty()
-
-        val createdPizzaDto = ObjectMapper().readValue(contentAsString, PizzaDto::class.java)
-        assertThat(createdPizzaDto).isEqualTo(
-            PizzaDto(
-                id = createdPizzaDto.id,
-                name = pizzaName,
-                type = pizzaType
+                    )
             )
-        )
+                .andExpect(status().isCreated)
+                .andReturn()
+
+            val contentAsString = mockMvcResult.response.contentAsString
+            assertThat(contentAsString).isNotEmpty()
+
+            val createdPizzaDto = ObjectMapper().readValue(contentAsString, PizzaDto::class.java)
+            assertThat(createdPizzaDto).isEqualTo(
+                PizzaDto(
+                    id = createdPizzaDto.id,
+                    name = pizzaName,
+                    type = pizzaType
+                )
+            )
+        }
+
+        @Test
+        fun `SHOULD try to create a pizza and return BAD REQUEST WHEN parameters are not valid`() {
+            mockMvc.perform(
+                post("/v1/pizzas")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(
+                        """
+                    {
+                        "type": "${NEAPOLITAN.name}"
+                    }
+                """.trimIndent()
+                    )
+            )
+                .andExpect(status().isBadRequest)
+                .andReturn()
+        }
     }
 }
