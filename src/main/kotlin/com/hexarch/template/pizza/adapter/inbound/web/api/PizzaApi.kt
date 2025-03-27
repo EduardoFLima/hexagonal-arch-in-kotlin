e3398587c585ccb11d25b8016c4f3dca72adc2e1
Edu Lima <eduefi@gmail.com>
2025-03-27 09:58:16 +0100

    remove postmapping from Api interface
    as it is not needed

--- src/main/kotlin/com/hexarch/template/pizza/adapter/inbound/web/api/PizzaApi.kt
+++ src/main/kotlin/com/hexarch/template/pizza/adapter/inbound/web/api/PizzaApi.kt
@@ -1,10 +1,8 @@
 package com.hexarch.template.pizza.adapter.inbound.web.api
 
 import com.hexarch.template.pizza.application.dto.PizzaDto
-import org.springframework.web.bind.annotation.PostMapping
 
 interface PizzaApi {
 
-    @PostMapping("/v1/pizzas")
     fun getPizza(pizzaDto: PizzaDto): PizzaDto
 }
